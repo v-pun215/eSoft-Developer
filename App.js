0b6420fa99d8d0d9db43eec2a237652288cbc43a
vihaanpundir <67716965+vihaanpundir@users.noreply.github.com>
2021-06-03 10:38:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,27 @@
+function download(filename, text) {
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + 
 
+encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+
+  pom.style.display = 'none';
+  document.body.appendChild(pom);
+
+  pom.click();
+
+  document.body.removeChild(pom);
+}
+
+function addTextHTML()
+{
+    document.addtext.name.value = document.addtext.name.value + ".html"
+}
+
+function addTextTXT()
+{
+    document.addtext.name.value = document.addtext.name.value + ".txt"
+}
 function compile() {
 
 	var html = document.getElementById("html");
